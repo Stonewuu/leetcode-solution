39e23fe08df74dde8224a64a8ec6463e6488b257
stonewu <me@stonewu.com>
2021-06-02 16:52:12 +0800

    题目3

---
+++ src/main/java/com/stonewu/solution/problem_3/Solution.java
@@ -0,0 +1,55 @@
+package com.stonewu.solution.problem_3;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * <p>
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 示例 4:
+ * <p>
+ * 输入: s = ""
+ * 输出: 0
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= s.length <= 5 * 104
+ * s 由英文字母、数字、符号和空格组成
+ */
+public class Solution {
+    public static int lengthOfLongestSubstring(String s) {
+        int start = 0, end = 0, length = 0;
+        int[] intMap = new int[128];
+        for (; end < s.length(); end++) {
+            char c = s.charAt(end);
+            if (intMap[c] > 0) {
+                start = intMap[c] > start ? intMap[c] : start;
+            }
+            length = length > end - start + 1 ? length : end - start + 1;
+            intMap[c] = end + 1;
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        lengthOfLongestSubstring("abcdabcd");
+    }
+}
